ea325a8fc33a7c381c3b59b4f7a1a514d49ebb96
Shawnpcw <s.cannon.williams@gmail.com>
2019-04-07 19:03:03 -0700

    first commit

---
+++ apps/time_display/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views     
+
+
+urlpatterns = [
+    url(r'^$', views.index) ,  # This line has changed! Notice that urlpatterns is a list, the comma is in
+    url(r'^time_display$', views.index)
+]   
